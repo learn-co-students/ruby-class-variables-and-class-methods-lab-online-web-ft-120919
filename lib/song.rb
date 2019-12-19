9b0637ca81c3d77d35932fdae512c7742baefafb
github username <github email address>
2019-12-19 14:03:05 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -22,24 +22,42 @@ class Song
     def self.artists
         @@artists.uniq
     end
-
+    
     def self.genres
         @@genres.uniq
     end
-
+    
     def self.genre_count
         genre_hash = {}
         @@genres.each do |genre|
-            
-        end
-    end
-    binding.pry
+=begin
+            if genre hash has a key of this genre
+                increase the count by one
+             else (genre hash does not know about this genre key yet)
+                create that key and start the value off at 1
+             end
+=end
+            # binding.pry
+            if genre_hash[genre]
+                genre_hash[genre] += 1
+            else 
+                genre_hash[genre] = 1
+            end #if
+        end #do
+        genre_hash
+    end #def
 
 
     def self.artist_count
         artist_hash = {}
-
-        @@artists.each {|artist| }
+        @@artists.each do |artist| 
+            if artist_hash[artist]
+                artist_hash[artist] += 1
+            else
+                artist_hash[artist] = 1
+            end
+        end
+        artist_hash
     end
 
 
